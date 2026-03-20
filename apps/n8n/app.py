0e071ee7ed13a73186c659ec412a98ed4c2f28a1
sudong.lee <mlambdasoo@gmail.com>
2026-03-20 14:33:35 +0900

    Cleanup project: remove unused files and artifacts.

--- apps/n8n/app.py
+++
@@ -1,6 +0,0 @@
-from utils.jsrunner import NodeJsEntrypoint
-
-# entrypoint for the n8n docker container
-if __name__ == "__main__":
-    entrypoint = NodeJsEntrypoint()
-    entrypoint.with_command("npx n8n").run()
--- sample/sample_python_project/tests/main_test.py
+++
@@ -1,6 +0,0 @@
-from sample_python_project.main import get_taxis, get_spark
-
-
-def test_main():
-    taxis = get_taxis(get_spark())
-    assert taxis.count() > 5
